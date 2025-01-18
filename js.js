739ae23e3c41a3ce45fca5269b41e2e5a5f845c8
Twullur <189119157+Twullur@users.noreply.github.com>
2025-01-18 22:06:38 +0700

    basic functionality done:drawing with black or random colors

--- js.js
+++ js.js
@@ -1,59 +1,99 @@
-/* PSEUDOCODE
-
-function makeSquares(x)
-    loop: creates 1 square x number of times 
-        square properties
-            height = canvas.length/x
-            width = height
-            background-color:white
-        append the square into the canvas       
-
-makeSquares(16);
-
-addeventlistener for button "click":
-    take an input on the size of the grid: (x*x)
-    makesSquares(x);
-    append the squares into the canvas
-
-forEach square:
-    addeventlistener "click"
-        square.background-color: black    
+let canvas = document.querySelector('.canvas');
+makeSquares(16); //default number of squares on the canvas
 
 
-function eraser 
-    square.background-color:white
+// Starting below, the code is presented in chronological order from the ones that were typed first.
 
-function clear
-    select all squares
-        background.color:white
-
-addevent listener click for eraser button
-
-addevent listener click for clear button
-
-*/
-
-let canvas = document.querySelector(".canvas");
-
-
-
-function makeSquares(numSquares) {
-    if (numSquares > 100) {
-        alert ("To prevent crashes, we have set a limit to 100 x 100 squares and will thus not process any larger amount")
-    }
-    for (let i = 0; i < numSquares * numSquares; i++) {
-        let canvasSquares = document.createElement("div");
-        canvasSquares.style.cssText = `            
-            border: black solid 1px;
-            height: ${canvas.clientHeight/numSquares}px; 
+// put the squares into the canvas
+function makeSquares (numSquares) { 
+    
+    canvas.innerHTML="";
+    for (let i = 0; i < numSquares * numSquares; i++) { 
+        let canvasSquares = document.createElement('div');
+        canvasSquares.style.cssText = `
+            border: lightgrey solid 1px;
+            height: ${canvas.clientHeight/numSquares}px;
             aspect-ratio: 1/1;
-            box-sizing:border-box;
-            background-color: white;`;
+            box-sizing: border-box;
+            background-color:white;
+            `;
         canvas.appendChild(canvasSquares);
+    }    
+};
+
+
+// generate a new canvas but putting new squares into it 
+let button = document.querySelector('.inputSection > button');
+let input = document.querySelector('.inputSection > input')
+
+button.addEventListener('click', () => {
+    let inputValue = input.value;
+    
+    if (inputValue > 100) {
+        alert('To prevent freezing, the maximum we have set is 100x100')
+    } else if (inputValue < 1) {
+        alert('Please enter a number between 1 to 100')
+    } else {
+        makeSquares(inputValue); // typeof inputValue at this point is still a string, but it works.
+        input.value = "";
+        input.focus();
+        drawing();
+        pageLoad++;
     }
+})
+
+
+
+drawing(); // call the drawing function for the default state
+
+function drawing() { // the drawing function colors the existing squares when there is a mousedown and mouseover (must be done in order, and then listens for mouseup to stop drawing)
+    
+    let appendedSquares = document.querySelectorAll('.canvas > div');
+    let isDrawing = false;
+    appendedSquares.forEach(square => {
+        square.addEventListener('mousedown', () => {
+                square.style.backgroundColor = getColor();
+                isDrawing = true;
+                console.log(isDrawing, "mousedown");
+            });
+
+        square.addEventListener('mouseover', () => {
+            if (isDrawing) {
+                square.style.backgroundColor =  getColor();
+                console.log(isDrawing, "mouseover")
+            }
+        })
+            
+        square.addEventListener('mouseup', () => {
+            isDrawing = false;
+            console.log(isDrawing,'mouseup');
+        })        
+    })
 }
 
-makeSquares(2);
+
+// setting the color selector
+let currentColorMode = 'black';
+
+function getColor () {
+    if (currentColorMode === 'random') {
+        return randomColor()}
+        else return 'black';
+}
+
+let randomColors = document.querySelector('.randomColors');
+randomColors.addEventListener('click', () => currentColorMode ='random');
+
+let defaultBlack = document.querySelector('.defaultBlack');
+defaultBlack.addEventListener('click', () => currentColorMode ='black');
 
 
 
+// functions to get random color
+function randomNumber(topNumber) {
+    return Math.floor(Math.random()*topNumber+1)
+}
+function randomColor() {
+    return `RGB(${randomNumber(255)},${randomNumber(255)},${randomNumber(255)})`;
+
+}
